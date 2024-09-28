88a8057d5bc7575871ff63bdcc62563722ab219c
alwingodly <alwinaswin123@gmail.com>
2024-09-28 16:06:19 +0530

    build

--- api/server.js
+++ api/server.js
@@ -1,18 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import connectDB from './config/db.js';
-import authRoute from './routes/authRoute.js';
-import adminRoute from './routes/adminRoute.js';
-import userRoute from './routes/userRoute.js';
-import logger from './utils/logger.js';
-import helmet from 'helmet';
-import errorHandler from './middlewares/errorHandlingMiddilewares/errrorHandler.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import adminRoute from "./routes/adminRoute.js";
+import userRoute from "./routes/userRoute.js";
+import logger from "./utils/logger.js";
+import helmet from "helmet";
+import errorHandler from "./middlewares/errorHandlingMiddilewares/errrorHandler.js";
+import path from "path";
 dotenv.config();
-
+app.use(express.static(path.join(__dirname, "../taskman/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../taskman/build", "index.html"));
+});
 const PORT = process.env.PORT || 7700;
 
 connectDB();
@@ -20,27 +23,29 @@ connectDB();
 const app = express();
 
 app.use(helmet());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET, POST, PUT, DELETE',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || 'your-default-secret',
+    secret: process.env.SESSION_SECRET || "your-default-secret",
     resave: false,
     saveUninitialized: true,
   })
 );
 
 // Routes
-app.use('/smartway/auth', authRoute);
-app.use('/smartway/admin',adminRoute);
-app.use('/smartway/user',userRoute);
+app.use("/smartway/auth", authRoute);
+app.use("/smartway/admin", adminRoute);
+app.use("/smartway/user", userRoute);
 
 // Error handling middleware
 app.use(errorHandler);
