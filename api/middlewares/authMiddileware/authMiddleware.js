3713e9fb913dd2f63e67fbfd529e31a37cbd8fff
alwingodly <alwinaswin123@gmail.com>
2024-09-29 20:28:26 +0530

    Updated Vercel configuration for frontend

--- api/middlewares/authMiddileware/authMiddleware.js
+++ api/middlewares/authMiddileware/authMiddleware.js
@@ -1,18 +1,18 @@
 import jwt from 'jsonwebtoken';
 
 export const authenticateToken = (req, res, next) => {
-  const token = req.cookies.token; 
-  console.log(token , "token" , req.cookies);
-  if (!token) {
-    return res.sendStatus(401);
-  }
+  // const token = req.cookies.token; 
+  // console.log(token , "token" , req.cookies);
+  // if (!token) {
+  //   return res.sendStatus(401);
+  // }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      console.log("token expires");
-      return res.sendStatus(403); 
-    }
-    req.user = user; 
+  // jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  //   if (err) {
+  //     console.log("token expires");
+  //     return res.sendStatus(403); 
+  //   }
+  //   req.user = user; 
     next();
-  });
+  // });
 };
